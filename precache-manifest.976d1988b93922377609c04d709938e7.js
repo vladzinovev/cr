457d22e6ff31cdbca89c85871e89ca7b7b997753
Vladislav  Zinovyev <vladozdeveloper@gmail.com>
2022-12-11 00:16:53 +0300

    run project

---
+++ precache-manifest.976d1988b93922377609c04d709938e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b0735dd6f5835c9c810",
+    "url": "/cr/static/css/main.ca2be313.chunk.css"
+  },
+  {
+    "revision": "1b0735dd6f5835c9c810",
+    "url": "/cr/static/js/main.1b0735dd.chunk.js"
+  },
+  {
+    "revision": "efc4f6de74067f068261",
+    "url": "/cr/static/js/1.efc4f6de.chunk.js"
+  },
+  {
+    "revision": "209f6ea846d1fb491ced",
+    "url": "/cr/static/js/runtime~main.209f6ea8.js"
+  },
+  {
+    "revision": "11cd21ce66d9c6adc2c1c028ebf6e83a",
+    "url": "/cr/index.html"
+  }
+];
